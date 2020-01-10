4b9afc84d3b8b3e11d79e049ae25abcbd5133773
Ira Villar <villarira@gmail.com>
2020-01-09 22:44:13 -0500

    car parts edit

--- scrape_starters/car_parts_starter.py
+++ scrape_starters/car_parts_starter.py
@@ -7,11 +7,6 @@ import re
 
 driver = webdriver.Chrome()
 driver.get("https://www.carousell.ph/")
-#?slt=null
-
-# Click review button to go to the review section
-#category_button = driver.find_element_by_xpath('//span[@class="padLeft6 cursorPointer"]')
-#category_button.click()
 
 tab = driver.find_element_by_xpath('//*[@id="root"]/div/div[4]/div[1]/div[1]/div/div[2]')
 tab.click()
@@ -24,26 +19,19 @@ csv_file = open('car_parts.csv', 'w', encoding='utf-8', newline='')
 writer = csv.writer(csv_file)
 
 index = 1
-# We want to start the first two pages.
-# If everything works, we will change it to while True
+
 while index <=15:
 	try:
 		print("Scraping chunk number " + str(index))
 		index = index + 1
-		# Find all the reviews. The find_elements function will return a list of selenium select elements.
-		# Check the documentation here: http://selenium-python.readthedocs.io/locating-elements.html
 		
 
 		listings = driver.find_elements_by_xpath('//div[@class="styles__cardContent___TpQXu"]')
-		#listings = driver.find_elements_by_xpath('//div[@class="styles__listingsWrapper___2RJeL"]')
-
-		# Iterate through the list and find the details of each review.
+		
 		for listing in listings:
-			# Initialize an empty dictionary for each review
 			
 			listings_dict = {}
 			
-			# To get the attribute instead of the text of each element, use 'element.get_attribute(href) for example'
 			try:
 				product = listing.find_element_by_xpath('.//p[@class="styles__text___1gJzw styles__colorUrbanGrey60___2rwkI styles__overflowNormal___mT74G styles__singleline___nCFol styles__textAlignLeft___lqg5e styles__weightSemibold___uxIDP desktop__sizeS___30RAN"]').text
 			except:
@@ -55,35 +43,15 @@ while index <=15:
 			#status = listing.find_element_by_xpath('//*[@id="root"]/div/div[3]/div[1]/div[2]/main/div[1]/div/div/div[1]/a[2]/p[4]').text
 			driver.execute_script("arguments[0].scrollIntoView();",listing)
 			
-			#print('Product = {}'.format(product))
-			#print('Price = {}'.format(price))
-			#print('User = {}'.format(user))
-			#print('='*50)
 			
-			# Use relative xpath to locate text, username, date_published, rating.
-			# Your code here
-
-			# Uncomment the following lines once you verified the xpath of different fields
-			#text = review.find_element_by_xpath('.//span[@class = "pad6 onlyRightPad"]').text
-			#rating = review.find_element_by_xpath('//*[@id="reviews"]/div/div/div[2]/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/span/span[3]/span[1]').text
-			
-			#driver.execute_script("arguments[0].scrollIntoView();",review)
 			listings_dict['product'] = product
 			listings_dict['price'] = price 
 			listings_dict['user'] = user
 			listings_dict['status'] = status
-			# review_dict['date_published'] = date_published
-			#review_dict['rating'] = rating
-
+			
 			writer.writerow(listings_dict.values())
 			
-			#print('Text={}'.format(text))
-			#print('Rating={}'.format(rating))
-
-		# We need to scroll to the bottom of the page because the button is not in the current view yet.
-		#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		#time.sleep(1)
-		# Locate the next button element on the page and then call `button.click()` to click it.
+			
 		try:
 			button = driver.find_element_by_xpath('//button[@class="styles__button___3dxOP desktop__button___2Hl0n styles__medium___3KEDn styles__outline___3AGrh desktop__outline___2UF39 styles__loadMore___yYAF4"]')
 			#time.sleep(1)
